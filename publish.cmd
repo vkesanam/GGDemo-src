fef387f3f5d1e885a109e7f3ed6252c922404630
VenkataSambasivaRao <sambakesanam@gmail.com>
2018-06-17 14:39:53 +0400

    Add project files.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=ggdemo-Web-Deploy.pubxml -p:Password=WfrGchJSazpqiYqvrQn3ZMEnz1fnkd8huZKiu1TooZCb51m0raw3HWuX8gkc
+
